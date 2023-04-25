1b6de305271ae9e88b43b91aa74ac35a4fc713f7
Ab-Ruh <abell.demere@gmail.com>
2023-04-25 23:09:20 +0300

    printchar

--- printchar.c
+++ printchar.c
@@ -1,21 +1,49 @@
-/**
-* printchar - Prints a single character to stdout
-* @c: The character to print
-*
-* Return: The number of characters printed
-*/
-int printchar(char c)
-{
-return (write(1, &c, 1));
-}
+#include "main.h"
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
-* printstr - Prints a string to stdout
-* @str: The string to print
+* _printf - Prints a formatted string to stdout
+* @format: The format string
 *
 * Return: The number of characters printed
 */
-int printstr(char *str)
+int _printf(const char *format, ...)
 {
-return (write(1, str, strlen(str)));
+int len = 0;
+va_list args;
+
+va_start(args, format);
+
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+char c = (char) va_arg(args, int);
+len += write(1, &c, 1);
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+len += write(1, str, strlen(str));
+}
+else if (*format == '%')
+{
+len += write(1, "%", 1);
+}
+}
+else
+{
+len += write(1, format, 1);
+}
+format++;
+}
+
+va_end(args);
+
+return (len);
 }
