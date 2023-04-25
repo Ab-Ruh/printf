3fb91d75a5c06bfe6b022048b70c4332f41eca9d
iyasu2 <iyasuforreal@gmail.com>
2023-04-25 22:30:26 +0300

    task1

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+int print_int(int a, int b);
+int print_char(char c);
+int print_str(char *str);
+int count_formats(char *input);
+
+#endif /* MAIN_H */
--- printint.c
+++ printint.c
@@ -25,7 +25,31 @@ int print_str(char *str)
 {
 	return (write(1, str, strlen(str)));
 }
+#include <unistd.h>
 
+/**
+ * print_int - Prints integers
+ * @a: int input
+ * @b: int input number of characters printed
+ * Return: The number of characters printed
+ */
+int print_int(int a, int b)
+{
+	char c;
+
+	if (a < 0)
+	{
+		write(1, "-", 1);
+		a = -1 * a;
+	}
+	if (a > 9)
+	{
+		print_int(a / 10, b);
+	}
+	c = '0' + (a % 10);
+	b += write(1, &c, 1);
+	return (b);
+}
 /**
  * _printf - Prints a formatted string to stdout
  * @format: The format string
