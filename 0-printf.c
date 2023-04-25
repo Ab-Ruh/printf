9c7fb88fb56c431ff9b023bc5bdc2390f3f30e6e
Ab-Ruh <abell.demere@gmail.com>
2023-04-25 20:54:12 +0300

    mult_functions

--- 0-printf.c
+++ 0-printf.c
@@ -5,58 +5,86 @@
 #include <stdio.h>
 
 /**
-* _printf - Prints a formatted string to stdout
-* @format: The format string
-*
-* Return: The number of characters printed
-*/
+ * print_char - Prints a single character to stdout
+ * @c: The character to print
+ *
+ * Return: The number of characters printed
+ */
+int print_char(char c)
+{
+    return write(1, &c, 1);
+}
+
+/**
+ * print_str - Prints a string to stdout
+ * @str: The string to print
+ *
+ * Return: The number of characters printed
+ */
+int print_str(char *str)
+{
+    return write(1, str, strlen(str));
+}
+
+/**
+ * print_int - Prints an integer to stdout
+ * @num: The integer to print
+ *
+ * Return: The number of characters printed
+ */
+int print_int(int num)
+{
+    char buf[32];
+    int len = sprintf(buf, "%d", num);
+    return write(1, buf, len);
+}
+
+/**
+ * _printf - Prints a formatted string to stdout
+ * @format: The format string
+ *
+ * Return: The number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-	int len = 0;
-	va_list args;
+    int len = 0;
+    va_list args;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				char c = (char) va_arg(args, int);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                char c = (char) va_arg(args, int);
+                len += print_char(c);
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                len += print_str(str);
+            }
+            else if (*format == 'd' || *format == 'i')
+            {
+                int num = va_arg(args, int);
+                len += print_int(num);
+            }
+            else if (*format == '%')
+            {
+                len += print_char('%');
+            }
+        }
+        else
+        {
+            len += print_char(*format);
+        }
+        format++;
+    }
 
-				len += write(1, &c, 1);
-				fflush(stdout);
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
+    va_end(args);
 
-				len += write(1, str, strlen(str));
-				fflush(stdout);
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(args, int);
-
-				len = print_int(num, len);
-				fflush(stdout);
-			}
-			else if (*format == '%')
-			{
-				len += write(1, "%", 1);
-				fflush(stdout);
-			}
-		}
-		else
-		{
-			len += write(1, format, 1);
-		}
-		format++;
-	}
-
-	va_end(args);
-
-	return (len);
+    return len;
 }
