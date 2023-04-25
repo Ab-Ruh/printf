de90e667b6c263cd7efdb60a843e708b59fd4509
Ab-Ruh <abell.demere@gmail.com>
2023-04-26 01:11:40 +0300

    u

--- 0-printf.c
+++ 0-printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
 
@@ -14,7 +15,6 @@ int len = 0;
 va_list args;
 
 va_start(args, format);
-
 while (*format)
 {
 if (*format == '%')
@@ -23,11 +23,13 @@ format++;
 if (*format == 'c')
 {
 char c = (char) va_arg(args, int);
+
 len += write(1, &c, 1);
 }
 else if (*format == 's')
 {
 char *str = va_arg(args, char *);
+
 len += write(1, str, strlen(str));
 }
 else if (*format == '%')
@@ -41,7 +43,6 @@ len += write(1, format, 1);
 }
 format++;
 }
-
 va_end(args);
 
 return (len);
