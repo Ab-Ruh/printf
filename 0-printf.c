a40806e66e1c16f4d656e1cd0c75989b3012e47f
Ab-Ruh <abell.demere@gmail.com>
2023-04-26 01:04:55 +0300

    git

--- 0-printf.c
+++ 0-printf.c
@@ -1,66 +1,48 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
-* _printf - Printf function
-* @format: format.
-* Return: Printed chars.
+* _printf - Prints a formatted string to stdout
+* @format: The format string
+*
+* Return: The number of characters printed
 */
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
+int len = 0;
+va_list args;
 
-if (format == NULL)
-return (-1);
+va_start(args, format);
 
-va_start(list, format);
-
-for (i = 0;  format && format[i] != '\0';  i++)
+while (*format)
 {
-if (format[i] != '%')
+if (*format == '%')
 {
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1); */
-printed_chars++;
+format++;
+if (*format == 'c')
+{
+char c = (char) va_arg(args, int);
+len += write(1, &c, 1);
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+len += write(1, str, strlen(str));
+}
+else if (*format == '%')
+{
+len += write(1, "%", 1);
+}
 }
 else
 {
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
+len += write(1, format, 1);
 }
+format++;
 }
 
-print_buffer(buffer, &buff_ind);
+va_end(args);
 
-va_end(list);
-
-return (printed_chars);
-}
-
-/**
-* print_buffer - Prints the contents of the buffer if it exist
-* @buffer: Array of chars
-* @buff_ind: Index at which to add next char, represents the length.
-*/
-void print_buffer(char buffer[], int *buff_ind)
-{
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-
-*buff_ind = 0;
+return (len);
 }
