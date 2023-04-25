5af949b329b6469b7ac548c7943dfb5609c3a3d5
Ab-Ruh <abell.demere@gmail.com>
2023-04-25 22:01:12 +0300

    printchar

--- printchar.c
+++ printchar.c
@@ -12,11 +12,9 @@
 int _printf(const char *format, ...)
 {
 int len = 0;
-
 va_list args;
 
 va_start(args, format);
-
 while (*format)
 {
 if (*format == '%')
@@ -25,11 +23,13 @@ format++;
 if (*format == 'c')
 {
 char c = (char) va_arg(args, int);
+
 len += write(1, &c, 1);
 }
 else if (*format == 's')
 {
 char *str = va_arg(args, char *);
+
 len += write(1, str, strlen(str));
 }
 else if (*format == '%')
@@ -43,8 +43,6 @@ len += write(1, format, 1);
 }
 format++;
 }
-
 va_end(args);
-
 return (len);
 }
