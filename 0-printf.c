ba446cc9eab0e76e8081ea12afeb5cb8feebe88a
Ab-Ruh <abell.demere@gmail.com>
2023-04-26 21:48:15 +0300

    push

--- 0-printf.c
+++ 0-printf.c
@@ -2,50 +2,48 @@
 #include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdio.h>
 
 /**
- * _printf - Prints a formatted string to stdout
- * @format: The format string
- *
- * Return: The number of characters printed
- */
+* _printf - Prints a formatted string to stdout
+* @format: The format string
+*
+* Return: The number of characters printed
+*/
 int _printf(const char *format, ...)
 {
-	int len = 0;
-	va_list args;
+int len = 0;
+va_list args;
 
-	va_start(args, format);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				print_char(args, len);
-			}
-			else if (*format == 's')
-			{
-				print_str(args, len);
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(args, int);
+va_start(args, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+char c = (char) va_arg(args, int);
 
-				print_int(num, len);
-			}
-			else if (*format == '%')
-			{
-				len += write(1, "%", 1);
-			}
-		}
-		else
-		{
-			len += write(1, format, 1);
-		}
-		format++;
-	}
-	va_end(args);
-	return (len);
+len += write(1, &c, 1);
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+
+len += write(1, str, strlen(str));
+}
+else if (*format == '%')
+{
+len += write(1, "%", 1);
+}
+}
+else
+{
+len += write(1, format, 1);
+}
+format++;
+}
+va_end(args);
+
+return (len);
 }
