5526853a4d0b93203b10bd6ace360d8d985387c5
Ab-Ruh <abell.demere@gmail.com>
2023-04-28 18:17:04 +0300

    betty

--- 0-printf.c
+++ 0-printf.c
@@ -1,52 +1,52 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format
- * @format: character string
- *
- * Return: number of characters printed
- */
+* _printf - produces output according to a format
+* @format: character string
+*
+* Return: number of characters printed
+*/
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int len = 0;
+va_list args;
+int len = 0;
 
-        va_start(args, format);
+va_start(args, format);
 
-        while (*format)
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        switch (*format)
-                        {
-                                case 'c':
-                                        putchar(va_arg(args, int));
-                                        len++;
-                                        break;
-                                case 's':
-                                        len += _puts(va_arg(args, char *));
-                                        break;
-                                case '%':
-                                        putchar('%');
-                                        len++;
-                                        break;
-                                default:
-                                        putchar('%');
-                                        putchar(*format);
-                                        len += 2;
-                                        break;
-                        }
-                }
-                else
-                {
-                        putchar(*format);
-                        len++;
-                }
-                format++;
-        }
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+putchar(va_arg(args, int));
+len++;
+break;
+case 's':
+len += _puts(va_arg(args, char *));
+break;
+case '%':
+putchar('%');
+len++;
+break;
+default:
+putchar('%');
+putchar(*format);
+len += 2;
+break;
+}
+}
+else
+{
+putchar(*format);
+len++;
+}
+format++;
+}
 
-        va_end(args);
+va_end(args);
 
-        return (len);
+return (len);
 }
