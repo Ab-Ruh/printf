f4831e324c9544211727f27ce082dd3d2c3594de
Ab-Ruh <abell.demere@gmail.com>
2023-04-28 18:12:19 +0300

    another try

--- 0-printf.c
+++ 0-printf.c
@@ -1,49 +1,52 @@
 #include "main.h"
-#include <string.h>
-#include <stdarg.h>
-#include <unistd.h>
 
 /**
-* _printf - Prints a formatted string to stdout
-* @format: The format string
-*
-* Return: The number of characters printed
-*/
+ * _printf - produces output according to a format
+ * @format: character string
+ *
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-int len = 0;
-va_list args;
+        va_list args;
+        int len = 0;
 
-va_start(args, format);
-if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-return (-1);
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'c')
-{
-char c = (char) va_arg(args, int);
-len += write(1, &c, 1);
-}
-else if (*format == 's')
-{
-char *str = va_arg(args, char *);
-len += write(1, str, strlen(str));
-}
-else if (*format == '%')
-{
-len += write(1, "%", 1);
-}
-}
-else
-{
-len += write(1, format, 1);
-}
-format++;
-}
-va_end(args);
+        va_start(args, format);
 
-return (len);
+        while (*format)
+        {
+                if (*format == '%')
+                {
+                        format++;
+                        switch (*format)
+                        {
+                                case 'c':
+                                        putchar(va_arg(args, int));
+                                        len++;
+                                        break;
+                                case 's':
+                                        len += _puts(va_arg(args, char *));
+                                        break;
+                                case '%':
+                                        putchar('%');
+                                        len++;
+                                        break;
+                                default:
+                                        putchar('%');
+                                        putchar(*format);
+                                        len += 2;
+                                        break;
+                        }
+                }
+                else
+                {
+                        putchar(*format);
+                        len++;
+                }
+                format++;
+        }
+
+        va_end(args);
+
+        return (len);
 }
